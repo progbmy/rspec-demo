58ffabcff53107d3af84d0ac85df77c5fe7f53e5
progbmy <skuter63@mail.ru>
2019-08-29 23:14:33 +0400

    Add car Class

---
+++ car.rb
@@ -0,0 +1,20 @@
+class Car
+  MILES_PER_GALLON = 20
+  attr_reader :fuel
+  def initialize
+    @fuel = 0
+  end
+
+  def add-fuel amount
+    @fuel += amount
+  end
+#Как далеко мы сможем проехать дистанция
+  def range
+    0
+  end
+end
+
+car = Car.new
+car.add_fuel 10
+
+puts "Rande is #{car.range}" # Мы сможем проехать столько то миль
